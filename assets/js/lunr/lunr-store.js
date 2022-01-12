e41b618676b847433a5715f71baa4e6a2a7c75d1
otch80 <48629275+otch80@users.noreply.github.com>
2022-01-12 18:53:47 +0900

    commit

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
         "title": "AMNN 논문리뷰",
-        "excerpt":" ","categories": ["머신러닝"],
+        "excerpt":"aaa  ","categories": ["AI"],
         "tags": ["ML"],
-        "url": "/%EB%A8%B8%EC%8B%A0%EB%9F%AC%EB%8B%9D/AMNN-%EB%85%BC%EB%AC%B8%EB%A6%AC%EB%B7%B0/",
+        "url": "/ai/AMNN/",
         "teaser": null
       }]
