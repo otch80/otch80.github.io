ba891b2a7f446ef26193b49336776f1b86477c26
otch80 <48629275+otch80@users.noreply.github.com>
2022-01-23 17:21:38 +0900

    recsys

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "RecSys Challenge 2015: ensemble learning with categorical features 리뷰",
+        "excerpt":"Yandex Data Factory 소속인 Peter Romov, Evgeny Sokolov가 작성한 대회논문 RecSys Challenge 2015: ensemble learning with categorical features를 기반으로 Recsys Challenge 2015를 공부해보려 합니다. 해당 팀은 최종 score 63,102 으로 전체 1위에 랭크하였으며, 2위 와는 약 2,000 점의 점수 차이를 나타내었습니다. 우선 2015년에 개최된 recsys challenge에 대해 개인적으로 분석한 내용을...","categories": ["recommend system"],
+        "tags": ["논문리뷰","recsys challenge"],
+        "url": "/recommend%20system/Recsys-challenge-2015/",
+        "teaser": null
+      },{
         "title": "AMNN : Attention-Based Multimodal Neural Network Model for Hashtag Recommendation 논문 정리",
         "excerpt":"이제 대학교 4학년으로 올라가는 학부생 수준이지만, 공부한 내용을 정리하기 위해 차근차근히 글을 작성해보려 합니다 첫 게시글로 2020년 IEEE에 publish된 AMNN 이라는 모델을 소개하는 논문을 공부한 글을 정리해보겠습니다. 1. 개요 SNS를 통해 수많은 게시글이 생성되고 있음 하지만 전체 게시글 중 해시태그를 다는 비중은 매우 적음 따라서 AMNN 모델을 활용하여 SNS 에...","categories": ["AI"],
         "tags": ["ML"],
@@ -29,15 +35,9 @@ var store = [{
         "url": "/ai/TSMS/",
         "teaser": null
       },{
-        "title": null,
-        "excerpt":"Yandex Data Factory 소속인 Peter Romov, Evgeny Sokolov가 작성한 대회논문 RecSys Challenge 2015: ensemble learning with categorical features를 기반으로 Recsys Challenge 2015를 공부해보려 합니다. 해당 팀은 최종 score 63,102 으로 전체 1위에 랭크하였으며, 2위 와는 약 2,000 점의 점수 차이를 나타내었습니다. 우선 2015년에 개최된 recsys challenge에 대해 개인적으로 분석한 내용을...","categories": [],
-        "tags": null,
-        "url": "/2022-01-12-Recsys-challenge-2015/",
-        "teaser": null
-      },{
-        "title": null,
-        "excerpt":"회귀 모델을 평가하는 방법으로는 MSE, MAE,RMSE 등 실제 값과 예측 값 사이의 오차를 활용하여 측정하는 다양한 방법이 존재합니다.   그렇다면 분류 모델을 평가하기 위한 방법으로는 어떤 것이 있을지 살펴보겠습니다.   1. Confusion Matrix  ","categories": [],
-        "tags": null,
-        "url": "/2022-01-20-confusion-matrix/",
+        "title": "TP/TN/FP/FN 분류 모델 성능 평가",
+        "excerpt":"회귀 모델을 평가하는 방법으로는 MSE, MAE,RMSE 등 실제 값과 예측 값 사이의 오차를 활용하여 측정하는 다양한 방법이 존재합니다.   그렇다면 분류 모델을 평가하기 위한 방법으로는 어떤 것이 있을지 살펴보겠습니다.   1. Confusion Matrix  ","categories": ["recommend system"],
+        "tags": ["Classifier","성능평가"],
+        "url": "/recommend%20system/confusion-matrix/",
         "teaser": null
       }]
