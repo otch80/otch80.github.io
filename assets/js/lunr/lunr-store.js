74ea780b3656207ddecf4ce4fbe0bb6555b8d7c8
otch80 <48629275+otch80@users.noreply.github.com>
2022-01-23 16:26:43 +0900

    recsys chall

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -18,9 +18,9 @@ var store = [{
         "teaser": null
       },{
         "title": "RecSys Challenge 2015: ensemble learning with categorical features 리뷰",
-        "excerpt":"Yandex Data Factory 소속인 Peter Romov, Evgeny Sokolov가 작성한 대회논문 RecSys Challenge 2015: ensemble learning with categorical features를 기반으로 Recsys Challenge 2015를 공부해보려 합니다. 해당 팀은 최종 score 63,102 으로 전체 1위에 랭크하였으며, 2위 와는 약 2,000 점의 점수 차이를 나타내었습니다. 우선 2015년에 개최된 recsys challenge에 대해 개인적으로 분석한 내용을...","categories": ["AI"],
+        "excerpt":"Yandex Data Factory 소속인 Peter Romov, Evgeny Sokolov가 작성한 대회논문 RecSys Challenge 2015: ensemble learning with categorical features를 기반으로 Recsys Challenge 2015를 공부해보려 합니다. 해당 팀은 최종 score 63,102 으로 전체 1위에 랭크하였으며, 2위 와는 약 2,000 점의 점수 차이를 나타내었습니다. 우선 2015년에 개최된 recsys challenge에 대해 개인적으로 분석한 내용을...","categories": ["IRLAB"],
         "tags": ["ML"],
-        "url": "/ai/Recsys-challenge-2015/",
+        "url": "/irlab/Recsys-challenge-2015/",
         "teaser": null
       },{
         "title": "SR 기반 모델 생성",
