9b465ac60278233b0e992dcf43795c36c5f6e96f
otch80 <48629275+otch80@users.noreply.github.com>
2022-01-26 16:01:15 +0900

    published false

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,16 +1,4 @@
 var store = [{
-        "title": "AMNN : Attention-Based Multimodal Neural Network Model for Hashtag Recommendation 논문 정리",
-        "excerpt":"2020년 IEEE에 publish된 AMNN 이라는 모델을 소개하는 논문을 공부한 글을 정리해보겠습니다. 1. 개요 SNS를 통해 수많은 게시글이 생성되고 있음 하지만 전체 게시글 중 해시태그를 다는 비중은 매우 적음 따라서 AMNN 모델을 활용하여 SNS 에 업로드 된 게시글에 맞춤형 해시태그를 추천하는 시스템을 설계 저자는 해시태그가 가지는 데이터에 초점을 맞추어 글을 작성하였습니다....","categories": ["recommend system"],
-        "tags": ["논문리뷰","Attention Mechanism"],
-        "url": "/recommend%20system/AMNN/",
-        "teaser": null
-      },{
-        "title": "TP/TN/FP/FN 분류 모델 성능 평가",
-        "excerpt":"회귀 모델을 평가하는 방법으로는 MSE, MAE,RMSE 등 실제 값과 예측 값 사이의 오차를 활용하여 측정하는 다양한 방법이 존재합니다.   그렇다면 분류 모델을 평가하기 위한 방법으로는 어떤 것이 있을지 살펴보겠습니다.   1. Confusion Matrix  ","categories": ["recommend system"],
-        "tags": ["Classifier","성능평가"],
-        "url": "/recommend%20system/confusion-matrix/",
-        "teaser": null
-      },{
         "title": "RecSys Challenge 2015: ensemble learning with categorical features 리뷰",
         "excerpt":"Yandex Data Factory 소속인 Peter Romov, Evgeny Sokolov가 작성한 대회논문 RecSys Challenge 2015: ensemble learning with categorical features를 기반으로 Recsys Challenge 2015를 공부해보려 합니다. 해당 팀은 최종 score 63,102 으로 전체 1위에 랭크하였으며, 2위 와는 약 2,000 점의 점수 차이를 나타내었습니다. 우선 2015년에 개최된 recsys challenge에 대해 개인적으로 분석한 내용을...","categories": ["recommend system"],
         "tags": ["논문리뷰","recsys challenge"],
