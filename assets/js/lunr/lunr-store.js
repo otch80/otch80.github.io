7342c8df1fd7fa492dda593eb220567d0659570b
otch80 <48629275+otch80@users.noreply.github.com>
2022-02-09 01:59:13 +0900

    여행지 추천

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "AMNN을 활용한 맞춤형 여행지 추천 서비스",
+        "excerpt":"안녕하세요 이번에는 AMNN 모델을 활용해서 제작한 여행지 추천 서비스에 대한 전반적인 설명과 프로젝트 진행하며 다루었던 여러 이슈들에 대해 포스팅해보려 합니다. 서비스 주제 사용자 맞춤형 여행지 추천 서비스 소개 초기 화면 서비스 진입 시 볼 수 있는 화면입니다 “당신이 선택한 사진을 통해 성공적인 여행을 책임진다” 는 뜻의 Picture + Victory 를...","categories": ["recommend system"],
+        "tags": ["여행지 추천","개인화 시스템","AMNN","추천 알고리즘"],
+        "url": "/recommend%20system/%EC%97%AC%ED%96%89%EC%A7%80%EC%B6%94%EC%B2%9C/",
+        "teaser": null
+      },{
         "title": "RecSys Challenge 2015: ensemble learning with categorical features 리뷰",
         "excerpt":"Yandex Data Factory 소속인 Peter Romov, Evgeny Sokolov가 작성한 대회논문 RecSys Challenge 2015: ensemble learning with categorical features를 기반으로 Recsys Challenge 2015를 공부해보려 합니다. 해당 팀은 최종 score 63,102 으로 전체 1위에 랭크하였으며, 2위 와는 약 2,000 점의 점수 차이를 나타내었습니다. 우선 2015년에 개최된 recsys challenge에 대해 개인적으로 분석한 내용을...","categories": ["recommend system"],
         "tags": ["논문리뷰","recsys challenge"],
